501e57773d80712d3c51f7e83a1daf09ff4e3f82
EmiTex <emad.armoun@gmail.com>
2024-02-22 12:37:41 +0100

    update sql queries

--- checks/database_queries.sql
+++ checks/database_queries.sql
@@ -3,6 +3,8 @@
 
 -- SELECT count(*) from teammate
 
+SELECT * from teammate
+
 -- SELECT * from supervisor LEFT OUTER JOIN role ON role.id = supervisor.roleId
 
-SELECT teammate.name, teammate.lastName, supervisor.* from supervisor LEFT OUTER JOIN teammate ON teammate.id = supervisor.teammateId
+-- SELECT teammate.name, teammate.lastName, supervisor.* from supervisor LEFT OUTER JOIN teammate ON teammate.id = supervisor.teammateId
